56d5f0eaf49d84b59cbc6515e6d9aafe8296ce8b
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-04-17 12:27:24 +0900

    Chore: 프론트 개발을 위한 JQuery 추가.

--- view/templates/header.php
+++ view/templates/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP CHAT</title>
-  <link rel="stylesheet" href="assets/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CHAT</title>
+    <link rel="stylesheet" href="assets/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 </head>
 <body>
-  <header>
+<header>
     <h1>PHP CHAT</h1>
-  </header>
+</header>
