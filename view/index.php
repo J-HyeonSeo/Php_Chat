676365ea61f8ebe6d425d10948a6c0680dc07007
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-04-17 13:04:44 +0900

    Feat: 모달 이벤트 처리.

--- view/index.php
+++ view/index.php
@@ -1,12 +1,12 @@
 <?php include 'templates/header.php'; ?>
 
-  <div id="back-drop"></div>
+  <div id="back-drop" onclick="closeModal()"></div>
 
   <!-- 신규 채팅방 생성 모달 -->
   <div class="modal" id="new-chat-modal">
     <h3 class="modal-title">새 채팅</h3>
     <div class="modal-input-wrap">
-      <span class="modal-close-btn">X</span>
+      <span class="modal-close-btn" onclick="closeModal()">X</span>
       <label for="chat-room-title">채팅방 제목</label>
       <br>
       <input id="chat-room-title" placeholder="제목을 입력해주세요...">
@@ -22,7 +22,7 @@
   <div class="modal" id="chat-enter-modal">
     <h3 class="modal-title">방 입장</h3>
     <div class="modal-input-wrap">
-      <span class="modal-close-btn">X</span>
+      <span class="modal-close-btn" onclick="closeModal()">X</span>
       <label for="nickname">닉네임</label>
       <br>
       <input id="nickname" placeholder="닉네임을 입력해주세요...">
@@ -33,21 +33,21 @@
   <div id="top-wrap">
     <span style="width: 130px;"></span>
     <h2 id="top-title">채팅방 목록</h2>
-    <div id="new-chat-btn">새 채팅..</div>
+    <div id="new-chat-btn" onclick="openNewChatModal()">새 채팅..</div>
   </div>
 
-  <div id="chat-room-wrap">
-    <div class="chat-room-card">
-      <h3 class="chat-title">심심해요... PHP로 대화할래요?</h3>
-      <h3 class="chat-count">1 / 4</h3>
-    </div>
-  </div>
+  <div id="chat-room-wrap"></div>
 
   <script>
 
       // 채팅방 목록을 가져오기 위한 웹소켓 서버 연결.
      const socket = new WebSocket(WEB_SOCKET_SERVER + "/chat/room");
+
+     // DOM 객체 변수.
      const chatRoomWrapEle = $('#chat-room-wrap');
+     const backDropEle = $('#back-drop');
+     const newChatModalEle = $('#new-chat-modal');
+     const chatEnterModalEle = $('#chat-enter-modal');
 
      // 채팅방 목록을 전달받았을 경우.
      socket.onmessage = function(event) {
@@ -59,18 +59,33 @@
          for (chatRoom of chatRooms) {
 
              const chatCardHtml = `
-                <div class="chat-room-card" data-uuid="${chatRoom.uuid}">
+                <div class="chat-room-card" onclick="openEnterModal()" data-uuid="${chatRoom.uuid}">
                   <h3 class="chat-title">${chatRoom.title}</h3>
                   <h3 class="chat-count">${chatRoom.count} / 4</h3>
                 </div>
               `;
 
              chatRoomWrapEle.append(chatCardHtml);
-
-             console.log(chatRoom);
          }
 
      }
+
+
+    function openNewChatModal() {
+        backDropEle.show();
+        newChatModalEle.show();
+    }
+
+    function openEnterModal() {
+        backDropEle.show();
+        chatEnterModalEle.show();
+    }
+
+    function closeModal() {
+         const modal = $('.modal');
+         modal.hide();
+         backDropEle.hide();
+    }
 
   </script>
 
